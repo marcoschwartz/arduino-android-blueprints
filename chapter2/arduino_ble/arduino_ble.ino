03c4ac57b68b0e1b76e5f29bc5a529e6f12e3391
marcoschwartz <marcolivier.schwartz@gmail.com>
2014-07-11 10:13:12 +0200

    chapter2 added

---
+++ chapter2/arduino_ble/arduino_ble.ino
@@ -0,0 +1,65 @@
+// Control Arduino board from BLE
+
+// Libraries
+#include <SPI.h>
+#include "Adafruit_BLE_UART.h"
+#include <aREST.h>
+
+// Pins
+#define ADAFRUITBLE_REQ 10
+#define ADAFRUITBLE_RDY 2     // This should be an interrupt pin, on Uno thats #2 or #3
+#define ADAFRUITBLE_RST 9
+
+// Create aREST instance
+aREST rest = aREST();
+
+// BLE instance
+Adafruit_BLE_UART BTLEserial = Adafruit_BLE_UART(ADAFRUITBLE_REQ, ADAFRUITBLE_RDY, ADAFRUITBLE_RST);
+
+void setup(void)
+{  
+  // Start Serial
+  Serial.begin(9600);
+  Serial.println(F("Adafruit Bluefruit Low Energy nRF8001 Print echo demo"));
+
+  // Start BLE
+  BTLEserial.begin();
+ 
+  // Give name and ID to device
+  rest.set_id("001");
+  rest.set_name("my_arduino");
+  
+  pinMode(6,OUTPUT);
+ 
+}
+
+aci_evt_opcode_t laststatus = ACI_EVT_DISCONNECTED;
+
+void loop() {  
+  
+  // Tell the nRF8001 to do whatever it should be working on.
+  BTLEserial.pollACI();
+  
+  // Ask what is our current status
+  aci_evt_opcode_t status = BTLEserial.getState();
+  // If the status changed....
+  if (status != laststatus) {
+    // print it out!
+    if (status == ACI_EVT_DEVICE_STARTED) {
+        Serial.println(F("* Advertising started"));
+    }
+    if (status == ACI_EVT_CONNECTED) {
+        Serial.println(F("* Connected!"));
+    }
+    if (status == ACI_EVT_DISCONNECTED) {
+        Serial.println(F("* Disconnected or advertising timed out"));
+    }
+    // OK set the last status change to this one
+    laststatus = status;
+  }
+  
+  // Handle REST calls
+  if (status == ACI_EVT_CONNECTED) {
+    rest.handle(BTLEserial);
+  }
+}
