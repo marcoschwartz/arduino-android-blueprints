d4a4cf04b6aa62628646bfd08a4ff6c7c88d1780
Stefan Buttigieg <info@stefanbuttigieg.com>
2014-09-12 13:11:46 +0200

    Android NFC App Updated
    The basics of the Android NFC App have been setup and connected. App
    needs to be tested with the NFC Shield.

--- chapter8/ArduinoNFC/app/src/main/java/com/arduinoandroid/arduinonfc/NFCActivity.java
+++ chapter8/ArduinoNFC/app/src/main/java/com/arduinoandroid/arduinonfc/NFCActivity.java
@@ -1,12 +1,25 @@
 package com.arduinoandroid.arduinonfc;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class NFCActivity extends Activity {
+
+    //Declaring the variable for mStatusText as a TextView
+    private TextView mStatusText;
+
+    //Getting the name for Log Tags
+    private final String DEBUG_TAG = NFCActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +27,49 @@ public class NFCActivity extends Activity {
         setContentView(R.layout.activity_nfc);
     }
 
+    @Override
+
+    public void onResume() {
+
+        super.onResume();
+
+        // Did we receive an NDEF message?
+
+
+        Intent intent = getIntent();
+
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(intent.getAction())) {
+
+            try {
+
+                Parcelable[] rawMsgs = intent
+
+                        .getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+
+
+                // we created the message, so we know the format
+
+                NdefMessage msg = (NdefMessage) rawMsgs[0];
+
+                NdefRecord[] records = msg.getRecords();
+
+                byte[] firstPayload = records[0].getPayload();
+
+                String message = new String(firstPayload);
+
+                mStatusText = (TextView) findViewById(R.id.nfcTextStatus);
+
+                mStatusText.setText(message);
+
+            } catch (Exception e) {
+
+                Log.e(DEBUG_TAG, "Error retrieving beam message.", e);
+
+            }
+
+        }
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
