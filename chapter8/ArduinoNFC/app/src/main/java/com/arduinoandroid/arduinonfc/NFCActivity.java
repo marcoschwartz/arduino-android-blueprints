7128e3997fc849e636d12fad5ed8710dd1b3592f
Stefan Buttigieg <info@stefanbuttigieg.com>
2014-09-12 15:57:45 +0200

    Changes to Android App
    Declared all the necessary classes to send and receive messages
    Work still needs to be done to make it workable with Arduino Project

--- chapter8/ArduinoNFC/app/src/main/java/com/arduinoandroid/arduinonfc/NFCActivity.java
+++ chapter8/ArduinoNFC/app/src/main/java/com/arduinoandroid/arduinonfc/NFCActivity.java
@@ -1,22 +1,38 @@
 package com.arduinoandroid.arduinonfc;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.nfc.NdefMessage;
 import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
+import android.nfc.NfcEvent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.os.Parcelable;
+import android.text.format.Time;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import java.nio.charset.Charset;
 
 
 public class NFCActivity extends Activity {
 
     //Declaring the variable for mStatusText as a TextView
     private TextView mStatusText;
+    private EditText messageToBeam;
+
+    //Initializing the NFC Adapater for sending messages
+    NfcAdapter mNfcAdapter;
+    private static final int BEAM_BEAMED = 0x1001;
+    public static final String MIMETYPE = "application/com.arduinoandroid.arduinonfc";
+
+    //Keys for Opening and Closing the Relay
+    String open_key = "oWnHV6uXre";
+    String close_key = "C19HNuqNU4";
 
     //Getting the name for Log Tags
     private final String DEBUG_TAG = NFCActivity.class.getSimpleName();
@@ -25,68 +41,91 @@ public class NFCActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nfc);
+
+        mStatusText = (TextView) findViewById(R.id.nfcTextStatus);
+        messageToBeam = (EditText) findViewById(R.id.sendView);
+
+        // Check for available NFC Adapter
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        if (mNfcAdapter == null) {
+            mStatusText.setText("NFC is not available on this device.");
+        }
+
+        // Register to create and NDEF message when another device is in range
+        mNfcAdapter.setNdefPushMessageCallback(new NfcAdapter.CreateNdefMessageCallback() {
+            @Override
+            public NdefMessage createNdefMessage(NfcEvent event) {
+                Time time = new Time();
+                time.setToNow();
+                String message = messageToBeam.getText().toString();
+                String text = (message + " \n[Sent @ "
+                        + time.format("%H:%M:%S") + "]");
+                byte[] mime = MIMETYPE.getBytes(Charset.forName("US-ASCII"));
+                NdefRecord mimeMessage = new NdefRecord(
+                        NdefRecord.TNF_MIME_MEDIA, mime, new byte[0], text
+                        .getBytes());
+                NdefMessage msg = new NdefMessage(
+                        new NdefRecord[] {
+                                mimeMessage,
+                                NdefRecord
+                                        .createApplicationRecord("com.arduinoandroid.arduinonfc") });
+                return msg;
+            }
+        }, this);
+
+        // And handle the send status
+        mNfcAdapter.setOnNdefPushCompleteCallback(
+                new NfcAdapter.OnNdefPushCompleteCallback() {
+
+                    @Override
+                    public void onNdefPushComplete(NfcEvent event) {
+                        mHandler.obtainMessage(BEAM_BEAMED).sendToTarget();
+                    }
+                }, this);
     }
 
+    @SuppressLint("HandlerLeak")
+    private final Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message message) {
+            switch (message.what) {
+                case BEAM_BEAMED:
+                    mStatusText.setText("Your message has been beamed");
+                    break;
+            }
+        }
+    };
+
     @Override
-
     public void onResume() {
-
         super.onResume();
-
         // Did we receive an NDEF message?
 
-
         Intent intent = getIntent();
-
         if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(intent.getAction())) {
-
             try {
-
                 Parcelable[] rawMsgs = intent
-
                         .getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
 
-
                 // we created the message, so we know the format
-
                 NdefMessage msg = (NdefMessage) rawMsgs[0];
-
                 NdefRecord[] records = msg.getRecords();
-
                 byte[] firstPayload = records[0].getPayload();
-
                 String message = new String(firstPayload);
-
-                mStatusText = (TextView) findViewById(R.id.nfcTextStatus);
-
                 mStatusText.setText(message);
-
             } catch (Exception e) {
-
                 Log.e(DEBUG_TAG, "Error retrieving beam message.", e);
-
             }
-
         }
-
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.nfc, menu);
-        return true;
+    public void onNewIntent(Intent intent) {
+        // handle singleTop so we don't launch a bunch of instances..
+        setIntent(intent);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
+
+
+
